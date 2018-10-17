d1f601fa6bd982311f47c27c7d447e945e4751a8
Edoardo Gargano <edoardo.gargano@yoox.com>
2018-10-17 20:05:51 +0200

    deploy

---
+++ precache-manifest.2bf0c40e263095368cc746598ba52031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/stores-map-vue/robots.txt"
+  },
+  {
+    "revision": "6efb222e6b9e5d181b01",
+    "url": "/stores-map-vue/js/chunk-vendors.bba87d20.js"
+  },
+  {
+    "revision": "3d3017d257a4872b5d97",
+    "url": "/stores-map-vue/js/app.a2e44bcd.js"
+  },
+  {
+    "revision": "23354afce5a855f8291a",
+    "url": "/stores-map-vue/js/about.d4a2497d.js"
+  },
+  {
+    "revision": "54f2cda79e538a557dda5d593ed9d758",
+    "url": "/stores-map-vue/index.html"
+  },
+  {
+    "revision": "3d3017d257a4872b5d97",
+    "url": "/stores-map-vue/css/app.a28d8758.css"
+  }
+];
